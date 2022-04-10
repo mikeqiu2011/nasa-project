6b8c42048e92bd4e06b65e71fa8d793375271d34
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-10 18:15:24 +0800

    export function in model, controller does not have to care detail

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,10 @@
-const { launches } = require('../../models/launches.model')
+const launchesModel = require('../../models/launches.model')
 
 function getAllLaunches(req, res) {
     // make sure only set response once, so add return so that function stops running
     // lauches is of type Map cannot be returned directly
-    return res.status(200).json(Array.from(launches.values()))
+    // return res.status(200).json(Array.from(launches.values()))
+    return res.status(200).json(launchesModel.getAllLaunches())
 }
 
 module.exports = {
