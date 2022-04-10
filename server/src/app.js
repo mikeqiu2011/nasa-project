5cca3e9098d196590278f331990ae3901416c39b
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-10 15:02:14 +0800

    cors middleware and default allow all

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express()
 
+app.use(cors()) // by default it allows all which is not secure
 app.use(express.json())
 app.use('/planets', planetsRouter) // now will get CORS blocked error
 
