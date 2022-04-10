fb3d2d851aaa7de96cf2fa4e0ad5f43a7407cbf7
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-10 15:05:15 +0800

    cors only allow our frontend

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,9 @@ const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express()
 
-app.use(cors()) // by default it allows all which is not secure
+app.use(cors({
+    origin: 'http://localhost:3000' // only allow from our frontend
+})) // by default it allows all which is not secure
 app.use(express.json())
 app.use('/planets', planetsRouter) // now will get CORS blocked error
 
