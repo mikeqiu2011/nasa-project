fe4a354064c7fa4726db6b42028daafaa71e1129
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-12 21:54:24 +0800

    refactor mongoDB connection code to separate

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,15 @@
 const http = require('http')
 const app = require('./app')
-const mongoose = require('mongoose')
-const PORT = process.env.PORT || 8000
-
-const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGO_PASS}@nasacluster.c9kez.mongodb.net/nasa?retryWrites=true&w=majority`
-
+const { mongoConnect } = require('./services/mongo')
 const { loadPlanetsData } = require('./models/planets.model')
+
+const PORT = process.env.PORT || 8000
 
 const server = http.createServer(app)
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready');
-})
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-})
-
 async function startServer() {
     // make sure data is ready before serving, a very common pattern
-    await mongoose.connect(MONGO_URL,)
+    await mongoConnect()
     await loadPlanetsData()
     server.listen(PORT, () => {
         console.log('server is listening on port', PORT);
