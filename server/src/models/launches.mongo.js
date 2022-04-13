346cd5b90d8f8ddb07f1334d493afbf2907efac7
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-13 22:31:48 +0800

    spaceX can save to mongo

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -19,7 +19,7 @@ const launchSchema = new mongoose.Schema({
     },
     destination: {
         type: String,
-        required: true,
+        // required: true,
     },
     upcoming: {
         type: Boolean,
