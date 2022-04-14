bc1bfc1913c4dcabe2a0955abdbfcf9885fc7cc6
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-14 20:16:08 +0800

    add default limit and page

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -1,6 +1,9 @@
+const DEFAULT_PAGE_LIMIT = 0  // default limit is all
+const DEFAULT_PAGE_NUMBER = 1
+
 function getPagination(query) {
-    const limit = Math.abs(query.limit)
-    const page = Math.abs(query.page)
+    const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT
+    const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER
 
     const skip = (page - 1) * limit
     return { limit, skip }
